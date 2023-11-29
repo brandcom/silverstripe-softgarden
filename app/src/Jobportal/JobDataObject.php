51e4d61c5b55ce89b10d0430b33cc0162ea90444
DanielVierheilig <dv@brandcom.de>
2023-11-29 10:22:02 +0100

    [TASK] add dataobject db fields for job

--- app/src/Jobportal/JobDataObject.php
+++ app/src/Jobportal/JobDataObject.php
@@ -10,4 +10,35 @@ use SilverStripe\ORM\DataObject;
  */
 class JobDataObject extends DataObject
 {
+    private static array $db = [
+        "jobDbId" => "Int",
+        "externalPostingName" => "Varchar",
+        "applyOnlineLink" => "Text",
+        "company_id" => "Varchar",
+        "company_name" => "Varchar",
+        "geo_city" => "Varchar",
+        "geo_country" => "Varchar",
+        "geo_lat" => "Varchar",
+        "geo_long" => "Varchar",
+        "geo_name" => "Varchar",
+        "geo_state" => "Varchar",
+        "geo_street" => "Varchar",
+        "geo_zip" => "Varchar",
+        "locale" => "Varchar",
+        "jobAdText" => "Text",
+        "jobStartDate" => "Int",
+        "job_ad_url" => "Varchar",
+        "job_owner_email" => "Varchar",
+        "job_owner_firstname" => "Varchar",
+        "job_owner_lastname" => "Varchar",
+        "job_owner_salutation" => "Varchar",
+        "job_owner_avatarurl" => "Varchar",
+        "postingLastUpdatedDate" => "Int",
+        "project_number" => "Varchar",
+        "employmentTypes" => "Varchar",
+        "industries" => "Varchar",
+        "internalJobAdText" => "Varchar",
+        "remote_status" => "Varchar",
+        "keywords" => "Varchar",
+    ];
 }
