8f891a5706e609827e7f8a96d0c0bf79cca5d9e1
DanielVierheilig <dv@brandcom.de>
2024-01-04 15:50:10 +0100

    [TASK] Modal Admin read only

--- src/JobModelAdmin.php
+++ src/JobModelAdmin.php
@@ -5,16 +5,34 @@ declare(strict_types=1);
 namespace brandcom\Softgarden;
 
 use SilverStripe\Admin\ModelAdmin;
+use SilverStripe\Forms\GridField\GridFieldConfig_RecordViewer;
 
 /**
  * Sorgt dafür, dass JobDataObjects im Silverstripe CMS angezeigt werden,
  * so dass man eine Job-Section hat in der Sidebar.
  */
+
 class JobModelAdmin extends ModelAdmin
 {
-    private static string $menu_title = "Job-Section";
+    private static $menu_title = "Job-Section";
+    private static $url_segment = "jobs";
+    private static $managed_models = [JobDataObject::class];
 
-    private static string $url_segment = "jobs";
+    public function getEditForm($id = null, $fields = null)
+    {
+        $form = parent::getEditForm($id, $fields);
 
-    private static array $managed_models = [JobDataObject::class];
+        if ($this->modelClass == JobDataObject::class) {
+            // Erstellung einer benutzerdefinierten GridField-Konfiguration für die Vorschau
+            $config = GridFieldConfig_RecordViewer::create();
+
+            // Einsetzen der benutzerdefinierten Konfiguration im GridField
+            $gridField = $form->Fields()->dataFieldByName($this->sanitiseClassName($this->modelClass));
+            if ($gridField) {
+                $gridField->setConfig($config);
+            }
+        }
+
+        return $form;
+    }
 }
