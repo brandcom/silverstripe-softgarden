e87d0ede2b3f3d84fd614e5aba785e5a372d5e97
DanielVierheilig <dv@brandcom.de>
2024-01-12 10:32:19 +0100

    [TASK] render import success content with auto redirect

--- src/JobImportBuildTask.php
+++ src/JobImportBuildTask.php
@@ -26,5 +26,34 @@ class JobImportBuildTask extends BuildTask
         $jobs = $client->getAllJobs();
         $newJobDataObject = new JobDataObject();
         $newJobDataObject->saveJobs($jobs);
+        echo "
+        <div style='font-family: sans-serif; 
+                background-color:lightgreen; 
+                padding: 100px 30px; 
+                width: 100%; 
+                height: 100vh;
+                font-size: 3.5vw; 
+                border-radius: 15px; 
+                box-shadow: 0 0 30px black; 
+                position: fixed; 
+                top: -40px; 
+                left: 0px;'>
+            <h2>Der Import war erfolgreich</h2>
+            <p id='lbl_back' style='font-size: 2vw; background: gray; padding: 20px; width: fit-content; color: white; border-radius: 8px;'>
+                Automatische Weiterleitung zurück erfolgt in 5 Sekunden
+            </p>
+        </div>";
+        
+        echo "<script>
+            const lbl_back = document.getElementById('lbl_back');
+            let time = 4;
+            setTimeout(() => {
+                window.location = '/admin/jobs';
+            }, 5000);
+            setInterval(() => {
+                lbl_back.innerHTML = 'Automatische Weiterleitung zurück erfolgt in ' + time + ' Sekunden';
+                time --;
+            }, 1000);
+        </script>";
     }
 }
